490a9409995fd435562467f6eaa35c10dc3dd19e
Nate Grebelsky <nate.greb@gmail.com>
2022-01-12 13:15:25 -0500

    update schema- uniquenesses, defaults

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -5,23 +5,24 @@ DROP TABLE IF EXISTS attribute;
 CREATE TABLE inventory (
     inventory_id INTEGER PRIMARY KEY,
     product_id INTEGER NOT NULL,
-    quantity INTEGER NOT NULL,
+    quantity INTEGER DEFAULT 1 NOT NULL,
     attribute_id INTEGER,
     product_price DECIMAL(10,2),
     FOREIGN KEY (product_id) REFERENCES product(product_id),
-    FOREIGN KEY (attribute_id) REFERENCES attribute(attribute_id)    
+    FOREIGN KEY (attribute_id) REFERENCES attribute(attribute_id),
+    UNIQUE(product_id, attribute_id)  
 );
 
 CREATE TABLE product (
     product_id INTEGER PRIMARY KEY,
-    product_name VARCHAR(255) NOT NULL,      
+    product_name VARCHAR(255) UNIQUE NOT NULL,      
     product_description TEXT,
     added TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE attribute (
     attribute_id INTEGER PRIMARY KEY,
-    attribute VARCHAR(255) NOT NULL
+    attribute_name VARCHAR(255) UNIQUE NOT NULL
 );
 
 -- Nosql might be better ... since attributes are dynamic
